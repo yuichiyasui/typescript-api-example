c3929f1a6651bc090cd1b06c91717b4049dca138
yuichiyasui <yassan.b.a.w@gmail.com>
2025-07-15 23:36:29 +0900

    test

--- apps/web/src/utils/env.ts
+++ apps/web/src/utils/env.ts
@@ -4,6 +4,8 @@ const envSchema = z.object({
   NEXT_PUBLIC_API_URL: z.string().min(1, "NEXT_PUBLIC_API_URL is required"),
 });
 
+console.log(process.env.NODE_ENV);
+
 type Env = z.infer<typeof envSchema>;
 
 const envObject: { [key in keyof Env]: string | undefined } = {
